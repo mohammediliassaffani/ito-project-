f461584a0bb9d840f21baa5bf5d0692d83d0742c
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-09-10 01:51:17 +0100

    create-order-migration

--- database/migrations/2023_09_09_204047_create_orders_table.php
+++ database/migrations/2023_09_09_204047_create_orders_table.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\user;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->decimal('total_price',20,2);
+            $table->string('status,45');
             $table->timestamps();
+            $table->foreignIdFor(user::class,'created_by')->nullable();
+            $table->foreignIdFor(user::class,'updated_by')->nullable();
         });
     }
 
