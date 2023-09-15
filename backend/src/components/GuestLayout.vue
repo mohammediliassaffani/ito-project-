0230d94fc222887be37812167eab53c641bc1c50
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-09-15 19:42:23 +0100

    login-fix

--- backend/src/components/GuestLayout.vue
+++ backend/src/components/GuestLayout.vue
@@ -1,23 +1,30 @@
 <template>
-    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div class="max-w-md w-full space-y-8">
-        <div>
-          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            {{ title }}
-          </h2>
+    <div
+        class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+    >
+        <div class="max-w-md w-full space-y-8">
+            <div>
+                <h2
+                    class="mt-6 text-center text-3xl font-extrabold text-gray-900"
+                >
+                    {{ title }}
+                </h2>
+            </div>
+            <form class="mt-8 space-y-6" @submit.prevent ="emit('submit')" method="POST">
+            <slot></slot>
+            </form>
         </div>
-        <slot></slot>
-      </div>
     </div>
-  </template>
-  
-  <script setup>
-    const {title} = defineProps({
-      title: String
-    })
-  </script>
-  
-  <style scoped>
-  
-  </style>
-  
+</template>
+
+<script setup>
+
+const { title } = defineProps({
+    title: String,
+});
+
+const emit = defineEmits(['submit'])
+
+</script>
+
+<style scoped></style>
